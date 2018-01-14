10af9139b683bf9e480d20c8bc4d7457d6ca7796
Sparsh <sparshgupta.a8@gmail.com>
2018-01-14 00:41:45 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-const express=require('express');
-const app=express();
+
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-const path=require('path');
 
-app.use('/',express.static(path.join(__dirname,'public_static')));
+var path = require('path');
+app.use('/',express.static(path.join(__dirname,'public')));
 
 io.on('connection',function(socket){
-	console.log(`user id: ${socket.id} connected` );
-	socket.emit('welcome',{
-		id:socket.id
-	});
-	socket.on('trigger',(data)=>{
-		io.emit('trigger');
+	console.log(`user-id: ${socket.id}`)
+	socket.broadcast.emit('newUser',{
+		text:'New User joined'
 	})
-})
+	socket.on('newMessage',function(data){
+		console.log(data);
+		io.emit('newMessage',{
+			text: data.text
+		})
+	})
 
-http.listen(3000, function(){
-  console.log('server started at http://localhost:3000/');
-});
+})
+http.listen(8000,function(){
+	console.log("Server started at port 8000");
+})
